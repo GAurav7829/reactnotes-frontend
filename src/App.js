90283a10a8e69f3c7b0a5b93831fb0c97a2b8070
Gaurav Singh <grvsgh7@gmail.com>
2021-06-17 21:38:48 +0530

    Completed add note feature

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NotesList from './components/NotesList';
 import NotFound from './components/NotFound';
 import Navbar from './components/Navbar';
+import AddNote from './components/AddNote';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <div>
           <Switch>
             <Route component={NotesList} path="/" exact />
+            <Route component={AddNote} path="/add" />
             <Route component={NotFound} path="*" />
           </Switch>
         </div>
